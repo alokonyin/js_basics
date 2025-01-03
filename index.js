cc60dcd8bdb604aa8a7a7ff512083cf8d2b05c31
Anyuon Ajak <146784917+alokonyin@users.noreply.github.com>
2025-01-02 21:40:31 -0500

    built a fully functioning passenger counter app

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.getElementById('counter').innerText = 5
+let countEl= document.getElementById('counter')
+let saveBtn = document.getElementById('save-btn')
+let saveEl = document.getElementById('saveEl')
+
+let count = 0
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+ 
+function save() {
+    let new_var = count + " - "
+    saveEl.textContent += new_var
+    countEl.textContent = 0
+    count = 0
+}
